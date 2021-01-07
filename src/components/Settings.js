9e1f4682a484a927ebab28d695b8f3d89dc9b03f
Filip <filipsrednicki42@gmail.com>
2021-01-07 20:42:41 +0100

    Added: form for password change

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,10 +1,37 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 const Settings = () => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const emailRef = useRef();
+  const passwordRef = useRef();
+  const confirmPassRef = useRef();
 
   const handleEmailChange = (e) => {
     e.preventDefault();
+  };
+
+  const handlePasswordChange = (e) => {
+    e.preventDefault();
+  };
+
+  const arePasswordsMatching = () => {
+    if (passwordRef.current.value === confirmPassRef.current.value || !confirmPassRef.current.value) {
+      confirmPassRef.current.style.boxShadow = "";
+    } else {
+      confirmPassRef.current.style.boxShadow = "0 0 5px 3px red";
+    }
+  }
+
+  const onPasswordInputChange = (e) => {
+    setPassword(e.target.value);
+    arePasswordsMatching()
+  };
+
+  const onConfirmPasswordInputChange = (e) => {
+    setConfirmPassword(e.target.value);
+    arePasswordsMatching()
   };
 
   return (
@@ -21,6 +48,35 @@ const Settings = () => {
           ref={emailRef}
         />
         <button type="submit">Change e-mail</button>
+      </form>
+
+      <form onSubmit={handlePasswordChange}>
+        <h2>Change your password</h2>
+        <label htmlFor="password">New Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          minLength="6"
+          placeholder="Password"
+          value={password}
+          onChange={onPasswordInputChange}
+          ref={passwordRef}
+        />
+        <label htmlFor="confirm-password">Confirm Password</label>
+        <input
+          type="password"
+          id="confirm-password"
+          name="password"
+          required
+          minLength="6"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={onConfirmPasswordInputChange}
+          ref={confirmPassRef}
+        />
+        <button type="submit">Change password</button>
       </form>
     </div>
   );
