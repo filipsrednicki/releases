d08d6fa98d18f1f3f786e1d217dbb1f7382249c8
Filip <filipsrednicki42@gmail.com>
2021-01-07 20:30:54 +0100

    Added: form for email change

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Settings = () => {
+  const emailRef = useRef();
+
+  const handleEmailChange = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="settings">
-      Settings
+      <form onSubmit={handleEmailChange}>
+        <h2>Change your e-mail</h2>
+        <label htmlFor="email">E-mail</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          placeholder="E-mail"
+          ref={emailRef}
+        />
+        <button type="submit">Change e-mail</button>
+      </form>
     </div>
   );
 };
