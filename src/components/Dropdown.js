cc73718b135555b2953846815da345b1ec270688
Filip <filipsrednicki42@gmail.com>
2021-01-09 00:32:33 +0100

    Added: Dropdown logic

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-const Dropdown = () => {
+const Dropdown = ({ children, setShowDropdown }) => {
+  const dropdownRef = useRef();
+
+  useEffect(() => {
+    const hideDropdown = (e) => {
+      if (
+        dropdownRef.current.contains(e.target) ||
+        e.target.classList.contains("dropdown-toggle")
+      ) {
+        return;
+      }
+      setShowDropdown(false);
+    };
+    document.addEventListener("mousedown", hideDropdown);
+
+    return () => {
+      document.removeEventListener("mousedown", hideDropdown);
+    };
+  }, [setShowDropdown]);
 
   return (
-    <div className="dropdown">
-
+    <div className="dropdown" ref={dropdownRef}>
+      {children}
     </div>
   );
 };
