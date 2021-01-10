3180591100b6cc8bafd4da3fdab23d785c4f50ab
Filip <filipsrednicki42@gmail.com>
2021-01-10 21:35:26 +0100

    Created: SearchError component

---
+++ src/components/Search/SearchError.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SearchError = ({ visible, isCategories, searchRef, children }) => {
+  if (!visible) {
+    return null;
+  }
+  return (
+    <div
+      className="search"
+      ref={searchRef}
+      style={isCategories && {
+        top: isCategories ? "-30px" : "-70px",
+        paddingTop: isCategories ? "9px" : "9px",
+      }}
+    >
+      <span className="search-error-msg">{children}</span>
+    </div>
+  );
+};
+
+export default SearchError;
