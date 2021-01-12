e505d1647a288b7db53760480b62ad9e9586b689
Filip <filipsrednicki42@gmail.com>
2021-01-12 23:37:50 +0100

    Changed: moved searchByTitle func into useSearch hook

---
+++ src/components/Search/useSearch.js
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const useSearch = (category) => {
+  const [results, setResults] = useState([]);
+  const [noResults, setNoResults] = useState(false);
+  const [error, setError] = useState(null);
+  const [loadingResults, setLoadingResults] = useState(false);
+
+  const history = useHistory();
+
+  const searchByTitle = (query, pageNum = 1) => {
+    setError(null);
+    setLoadingResults(true);
+
+    if (history.location.pathname.includes("/search")) {
+      history.push({
+        pathname: `/search/${category}`,
+        search: "?query=" + query,
+      });
+    }
+
+    let url = "";
+    if (category === "game") {
+      url = `https://api.rawg.io/api/games?page_size=5&page=${pageNum}&search=${query}`;
+    } else {
+      url = `https://api.themoviedb.org/3/search/${category}?api_key=${process.env.REACT_APP_TMDB_API_KEY}&query=${query}&page=${pageNum}`;
+    }
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.results.length === 0) {
+          setNoResults(true);
+          setLoadingResults(false);
+        } else {
+          setNoResults(false);
+          if (category === "tv") {
+            unifyResults(result, "t");
+          } else if (category === "game") {
+            unifyResults(result, "g");
+          } else {
+            unifyResults(result, "m");
+          }
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoadingResults(false);
+      });
+  };
+
+  const unifyResults = (result, prefix) => {
+    let resultsCopy = result.results.slice(0, 5);
+
+    if (prefix !== "g") {
+      resultsCopy = resultsCopy.map((result) => {
+        if (prefix === "m") {
+          const { title: name, release_date: released, ...rest } = result;
+          return { name, released, ...rest };
+        } else {
+          const { first_air_date: released, ...rest } = result;
+          return { released, ...rest };
+        }
+      });
+    }
+
+    resultsCopy.forEach((item, i) => {
+      item.id = prefix + item.id;
+      if (prefix === "g") {
+        if (item.platforms === null) {
+          item.platforms = [];
+        } else {
+          let platformsString = [];
+          item.platforms.slice(0, 3).forEach((platform) => {
+            platformsString.push(platform.platform.name);
+          });
+          item.platforms = platformsString;
+        }
+      }
+      if (prefix === "t") {
+        item.released = item.released.slice(0, 4);
+      }
+    });
+
+    resultsCopy.maxPages = result.total_pages
+      ? result.total_pages
+      : Math.round(result.count / 20);
+    setResults(resultsCopy);
+    setLoadingResults(false);
+  };
+
+  return {
+    searchByTitle,
+    results,
+    setResults,
+    noResults,
+    setNoResults,
+    error,
+    setError,
+    loadingResults,
+  };
+};
+
+export default useSearch;
