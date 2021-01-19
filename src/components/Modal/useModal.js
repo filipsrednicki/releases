694831ee4631779df643c69ed763450000afe031
Filip <filipsrednicki42@gmail.com>
2021-01-20 00:07:55 +0100

    Added: ability to close modal by pressing Escape key

--- src/components/Modal/useModal.js
+++ src/components/Modal/useModal.js
@@ -3,14 +3,21 @@ import { useEffect } from "react";
 const useModal = (changeDisplay) => {
   useEffect(() => {
     document.body.style.overflow = "hidden";
-    const hideForm = (e) => {
+    const handleMouse = (e) => {
       if (e.target.className === "modal") {
         changeDisplay();
       }
     };
-    document.addEventListener("mousedown", hideForm);
+    const handleKey = (e) => {
+      if(e.key === "Escape") {
+        changeDisplay();
+      }
+    }
+    document.addEventListener("mousedown", handleMouse);
+    document.addEventListener("keydown", handleKey);
     return () => {
-      document.removeEventListener("mousedown", hideForm);
+      document.removeEventListener("mousedown", handleMouse);
+      document.removeEventListener("keydown", handleKey);
       document.body.style.overflow = "";
     };
   }, [changeDisplay]);
