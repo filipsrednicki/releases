c7d1391a11443bc902975875c3f5a3a96ddd9bf5
Filip <filipsrednicki42@gmail.com>
2021-01-20 17:09:32 +0100

    Updated: Image component to use lazy load hook

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
 import img300 from "../icons/img-not-found-300x450.png";
 import img400 from "../icons/img-not-found-400x220.png";
+import useLazyLoad from "./useLazyLoad";
 
-const Image = ({ entry, width, isGameImg }) => {
+const Image = ({ entry, size = {w: 0, h: 0}, wrapperW = 100, isGameImg }) => {
   const [errorImgs, setErrorImgs] = useState([]);
+  const { targetRef } = useLazyLoad("0px 0px 100px 0px");
 
   const imgError = (id) => {
     setErrorImgs((prevState) => prevState.concat(id));
   };
 
-  let notFoundImgW = img300
-  if(isGameImg) {
-    notFoundImgW = img400
+  let notFoundImgW = img300;
+  if (isGameImg) {
+    notFoundImgW = img400;
+  }
+
+  let padding = false;
+  if(size) {
+    padding = (size.h / size.w) * wrapperW + "%";
   }
 
   return (
-    <>
+    <div
+      className="img-wrapper"
+      style={{ paddingBottom: padding ? padding : "100%", width: wrapperW + "%" }}
+    >
       {!errorImgs.includes(entry.id) ? (
         <img
+          ref={targetRef}
+          data-src={
+            entry.background_image ||
+            "https://image.tmdb.org/t/p/w" + size.w + entry.poster_path
+          }
           alt={entry.title || entry.name}
-          src={entry.background_image || "https://image.tmdb.org/t/p/w" + width + entry.poster_path}
           onError={() => imgError(entry.id)}
         />
       ) : (
@@ -28,7 +42,7 @@ const Image = ({ entry, width, isGameImg }) => {
           src={notFoundImgW}
         />
       )}
-    </>
+    </div>
   );
 };
 
