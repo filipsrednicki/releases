aedb29a5de736a5f9508a4b95791d5cea451928a
Filip <filipsrednicki42@gmail.com>
2021-01-20 17:14:56 +0100

    Added: placeholder element while image is loading

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,12 +3,21 @@ import img300 from "../icons/img-not-found-300x450.png";
 import img400 from "../icons/img-not-found-400x220.png";
 import useLazyLoad from "./useLazyLoad";
 
-const Image = ({ entry, size = {w: 0, h: 0}, wrapperW = 100, isGameImg }) => {
+const Image = ({ entry, size = { w: 0, h: 0 }, wrapperW = 100, isGameImg }) => {
+  const [loadedImgs, setLoadedImgs] = useState([]);
   const [errorImgs, setErrorImgs] = useState([]);
   const { targetRef } = useLazyLoad("0px 0px 100px 0px");
 
   const imgError = (id) => {
     setErrorImgs((prevState) => prevState.concat(id));
+  };
+
+  const isOpaque = (id) => {
+    if (loadedImgs.includes(id)) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   let notFoundImgW = img300;
@@ -17,14 +26,17 @@ const Image = ({ entry, size = {w: 0, h: 0}, wrapperW = 100, isGameImg }) => {
   }
 
   let padding = false;
-  if(size) {
+  if (size) {
     padding = (size.h / size.w) * wrapperW + "%";
   }
 
   return (
     <div
       className="img-wrapper"
-      style={{ paddingBottom: padding ? padding : "100%", width: wrapperW + "%" }}
+      style={{
+        paddingBottom: padding ? padding : "100%",
+        width: wrapperW + "%",
+      }}
     >
       {!errorImgs.includes(entry.id) ? (
         <img
@@ -35,13 +47,23 @@ const Image = ({ entry, size = {w: 0, h: 0}, wrapperW = 100, isGameImg }) => {
           }
           alt={entry.title || entry.name}
           onError={() => imgError(entry.id)}
+          onLoad={() => setLoadedImgs((l) => l.concat(entry.id))}
+          style={{ opacity: !isOpaque(entry.id) ? 0 : 1 }}
         />
       ) : (
         <img
           alt={"Image not found for " + entry.title || entry.name}
           src={notFoundImgW}
+          onLoad={() => setLoadedImgs((l) => l.concat(entry.id))}
+          style={{ opacity: !isOpaque(entry.id) ? 0 : 1 }}
         />
       )}
+      <div
+        className="placeholder"
+        style={{
+          opacity: isOpaque(entry.id) ? 0 : 1,
+        }}
+      ></div>
     </div>
   );
 };
