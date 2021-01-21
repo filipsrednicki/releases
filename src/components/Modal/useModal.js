fdfcc85592d0785e56d6ff75e826a821d866c01a
Filip <filipsrednicki42@gmail.com>
2021-01-21 22:39:37 +0100

    Fixed: overflow issue with 2 modals on top of each other

--- src/components/Modal/useModal.js
+++ src/components/Modal/useModal.js
@@ -2,22 +2,32 @@ import { useEffect } from "react";
 
 const useModal = (changeDisplay) => {
   useEffect(() => {
-    document.body.style.overflow = "hidden";
+    const bodyEl = window.getComputedStyle(document.body);
+    const bodyOverflow = bodyEl.getPropertyValue("overflow");
+    let isOverflowHidden = false;
+
+    if (bodyOverflow === "hidden") {
+      isOverflowHidden = true;
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+
     const handleMouse = (e) => {
       if (e.target.className === "modal") {
         changeDisplay();
       }
     };
     const handleKey = (e) => {
-      if(e.key === "Escape") {
+      if (e.key === "Escape") {
         changeDisplay();
       }
-    }
+    };
     document.addEventListener("mousedown", handleMouse);
     document.addEventListener("keydown", handleKey);
     return () => {
       document.removeEventListener("mousedown", handleMouse);
       document.removeEventListener("keydown", handleKey);
+      if (isOverflowHidden) return;
       document.body.style.overflow = "";
     };
   }, [changeDisplay]);
